38277c5bea61fc6cdc41d612522aba2c0a2cce88
Nicolas Rodriguez Salguero <nirodriguezsa@unal.edu.co>
2021-04-09 21:19:07 -0500

    Cambio en la manera que toma los numeros, actualización de BD

--- Bot.py
+++ Bot.py
@@ -3,18 +3,23 @@ import webbrowser as web
 import time as tm
 import pandas as pd
 
-data = pd.read_csv("Test.csv")
+data = pd.read_csv("datos.csv")
 data_dict = data.to_dict('list')
 celulares = data_dict['celular']
 first = True
 
+
 for celular in celulares:
-    web.open("https://api.whatsapp.com/send?phone="+celular+"&text=Hola que tal")
-    if first:
-        tm.sleep(3)
-        first=False
-    tm.sleep(8)
-    pg.press("enter")
+    web.open("https://api.whatsapp.com/send?phone=+57"+str(celular)+"&text=¡Hola! Soy Nicolas asesor de Tigo, nos enteramos que pasaste tu línea con otro operador _Claro_, y nos encantaría que volvieras! es por ello que te damos una excelente oferta con planes desde *$35.000* con 15 GB de internet y 2 meses al 50% de descuento. Si te interesa escríbeme y te cuento los detalles!")
     tm.sleep(2)
+    if first:
+        tm.sleep(8)
+        first=False
+    tm.sleep(4)
+    pg.press("enter")
+    tm.sleep(1)
+    pg.hotkey('alt','tab')
+    tm.sleep(0.2)
     pg.hotkey('ctrl', 'w')
+
 print("Script ejecutado correctamente")
