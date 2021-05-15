5029a15478f62ebd3ea4b7a4949648ccbb9e9c12
Nicolas Rodriguez Salguero <nirodriguezsa@unal.edu.co>
2021-05-14 21:59:20 -0500

    Ajuste a el modo en que abre la ventana, usa por defecto Google Chrome para su ejecucion

--- BotTriple.py
+++ BotTriple.py
@@ -3,16 +3,18 @@ import webbrowser as web
 import time as tm
 import pandas as pd
 
+chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
 data = pd.read_csv("datos.csv")
 data_dict = data.to_dict('list')
 celulares = data_dict['celular']
+nombres = data_dict['nombre']
 direcciones = data_dict['direccion']
-cedulas = data_dict['cedula']
-informacion = zip(celulares,direcciones,cedulas)
+informacion = zip(celulares,nombres,direcciones)
 first = True
 
-for celular,direcciones,cedulas in informacion:
-    web.open("https://api.whatsapp.com/send?phone=+57"+str(celular)+"&text=¡Hola!, mi nombre es Nicolas, asesor de Tigo, su servicio hogar instalado en la dirección "+direcciones+" bajo cédula "+str(cedulas)+" tiene un un *beneficio único*, que consiste en adquirir un plan móvil con nosotros, con el primer mes gratis y el doble de megas en planes desde 45 mil pesos con ¡*50 GB* y minutos ilimitados! Escríbame si desea conocer más acerca de este beneficio o si le gustaría que un asesor se comunicara para darle todos los detalles.")
+for celular,nombre,direccion in informacion:
+    url = "https://api.whatsapp.com/send?phone=+57"+str(celular)+"&text=¡Hola "+nombre+"! Mi nombre es Nicolas, asesor de ventas Tigo, quiero contarte que tienes una _super oferta_ para que actives un plan pospago con tu línea "+str(celular)+" ya que por tener los servicios de hogar en la dirección "+direccion+", te damos *doble de GIGAS* en el plan móvil que tomes desde 45 mil pesos con el *PRIMER MES GRATIS* y 2 meses *al 50% de descuento*. También recibes una promo para tu hogar y es que *duplicamos tus MB* de velocidad sin costo adicional! Escríbeme si te interesa esta oferta"
+    web.get(chrome_path).open(url)
     tm.sleep(2)
     if first:
         tm.sleep(8)
